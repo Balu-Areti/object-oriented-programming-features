e17187f1505f2c8dbab08abdfa405b56babb4f38
balu1006 <balumahendraareti@gmail.com>
2026-09-13 16:46:27 +0530

    Abstraction (The heart of OOP Design )

--- src/com/java/oop/interfaces/Main.java
+++ src/com/java/oop/interfaces/Main.java
@@ -8,8 +8,8 @@ public class Main {
         UPIPayment upiPayment1 = new UPIPayment(111,111,100,"12NOV");
         DebitCardPayment debitCardPayment = new DebitCardPayment(111,123,1234,1012345, 10);
 
-//        PaymentSevice paymentSevice = new PaymentSevice(debitCardPayment);
-//        paymentSevice.pay();
+        PaymentSevice paymentSevice = new PaymentSevice(debitCardPayment);
+        paymentSevice.pay();
 
         PaymentSevice Ps = new PaymentSevice(upiPayment1);
         Ps.pay();
